784af5f044eeacefc0101a83b006fdfbf86fcca9
ShinKeryo <tlswngud002@naver.com>
2024-01-09 18:05:26 +0900

    2024-01-09

---
+++ JSPMyWeb/src/main/java/com/project/customer/service/Service.java
@@ -0,0 +1,11 @@
+package com.project.customer.service;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public interface Service {
+	
+	public int customerJoin(HttpServletRequest request,HttpServletResponse response);
+	
+	
+}
